770c9b6d054f9c1d64d1bd2eb7dd3bff03c6923e
AndyMan <josueandy5@gmail.com>
2026-04-12 18:00:32 +0200

    debut test v1

---
+++ src/main/java/com/andy/gstockapi/GStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.andy.gstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GStockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GStockApiApplication.class, args);
+    }
+
+}
